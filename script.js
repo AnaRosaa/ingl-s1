4e99f0126e5560f6e28dd8de5e63aa177ab2ee95
anarosaa <silvaanarosa103@gmail.com>
2022-06-30 19:50:44 -0300

    Primeiro trabalho de inglês

---
+++ script.js
@@ -0,0 +1,44 @@
+/*Crie 3 variáveis, cada uma com um array:
+- Comedy
+-Action
+-Fantasy 
+
+/*Dentro de cada array, insira 3 (TRÊS) Objetos, contendo:
+-title:
+-director:
+-year:*/
+
+
+let Comedy = {
+    title:'Shrek',
+    director:'Andrew Adamson, Vicky Jenson',
+    year:2001
+}
+console.log(Comedy);
+
+let Action = {
+    title:'spider-man no way home',
+    director:'Jon Watts',
+    year:2021
+}
+console.log(Action);
+
+let Fantasy = {
+    title:'Beauty and the Beast',
+    director:'Bill Condon',
+    year:2017
+}
+console.log(Fantasy);
+
+
+
+/*Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
+IF: "MY FAVORIT MOVIE IS..."
+ELSE: "NOT EVEN 5 STARS..."*/
+
+let myMovie = true;
+if (myMovie) {
+    console.log('My favorit movie is spider-man no way home');
+} else {
+    console.log('Not even 5 stars The Kissing Booth');
+}
